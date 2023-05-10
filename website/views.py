46bc6860bcdcd99ec86ed0cb2ecffa5a9484f13e
Samuel <127067590+Panda5m@users.noreply.github.com>
2023-05-10 18:28:20 +1000

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect,url_for
+from .models import Events
 
 bp = Blueprint('main', __name__)
 
-
 @bp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    destinations = Events.query.all()    
+    return render_template('index.html', event=event)
+
+@bp.route('/search')
+def search():
+    if request.args['search']:
+        print(request.args['search'])
+        dest = "%" + request.args['search'] + '%'
+        event = Events.query.filter(Events.description.like(dest)).all()
+        return render_template('index.html', event=event)
+    else:
+        return redirect(url_for('main.index'))
