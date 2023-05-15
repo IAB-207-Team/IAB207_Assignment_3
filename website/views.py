25853f8e1a94cf554eccd55405b0db788bf998f5
Samuel <127067590+Panda5m@users.noreply.github.com>
2023-05-16 09:33:45 +1000

    Update views.py

--- website/views.py
+++ website/views.py
@@ -13,7 +13,7 @@ def search():
     if request.args['search']:
         print(request.args['search'])
         dest = "%" + request.args['search'] + '%'
-        event = Events.query.filter(Events.description.like(dest)).all()
+        event = Events.query.filter(Events.description.like(even)).all()
         return render_template('index.html', event=event)
     else:
         return redirect(url_for('main.index'))
