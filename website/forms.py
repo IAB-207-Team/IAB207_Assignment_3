e6a576b7f56985c6a589e75f1cad77252ca5bcbf
n11283173 <n11283173@qut.edu.au>
2023-05-16 09:51:14 +1000

    Add forms for creating a new event and comments

--- website/forms.py
+++ website/forms.py
@@ -2,6 +2,20 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+ALLOWED_FILE = {'PNG','JPG','png','jpg'}
+
+#Create new event
+class EventForm(FlaskForm):
+  name = StringField('Country', validators=[InputRequired()])
+  description = TextAreaField('Description', 
+            validators=[InputRequired()])
+  image = FileField('Destination Image', validators=[
+    FileRequired(message='Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
+  currency = StringField('Currency', validators=[InputRequired()])
+  submit = SubmitField("Create")
 
 
 #creates the login information
@@ -21,3 +35,7 @@ class RegisterForm(FlaskForm):
 
     #submit button
     submit = SubmitField("Register")
+
+class CommentForm(FlaskForm):
+    text = TextAreaField("Comment", validators=[InputRequired(), Length(min=10, max=200, message="Comment should be between 10 and 200 characters")]) 
+    submit = SubmitField("Add Comment")
