43d88e5aa006d67f476c701cfe2eafde99efbc02
Samuel <127067590+Panda5m@users.noreply.github.com>
2023-05-19 18:41:41 +1000

    Added the Booking table to the database

--- website/models.py
+++ website/models.py
@@ -23,13 +23,12 @@ class Event(db.model):
     genre = Column(String(255))
     location = Column(String(255))
     Amount_of_Tickets = Column(Integer)
-    Ticket_Status = Column(String(255))
-    Ticket_Price = Column(Integer)
+    event_Status = Column(String(255))
+    ticket_Price = Column(Integer)
     image = Column(String(400))
-    
-    comments = relationship('Comment', backref='event')
-    event_booking = relationship('Booking', backref='event_id')
 
+    comments = relationship('Comment', backref='event')
+    
 class Comment(db.model):
     __tablename__ = 'comments'
 
@@ -37,18 +36,21 @@ class Comment(db.model):
     text = Column(String(400))
     created_at = Column(DateTime, server_default='CURRENT_TIMESTAMP')
     user_id = Column(Integer, ForeignKey('users.id'))
-    event_id = Column(Integer, ForeignKey('event.id'))
+    event_id = Column(Integer, ForeignKey('events.id'))
 
     user = relationship('User', backref='comments')
 
 #Need to Add a booked table to keep track of tickets
 class Booking(db.model):
     __tablename__ = 'booking'
-    order_id = Column(Integer)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    event_id = Column(Integer, ForeignKey('event.id'))
     
+    order_id = Column(Integer)
+    order_date = Column(Date)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    event_id = Column(Integer, ForeignKey('events.id'))
+
     user_booking = relationship('User', backref='booking')
+    event_booking = relationship('Event', backref='booking')
 
     def __repr__(self):
         return "<Comment: {}>".format(self.text)
