9768826a08af2c0d74e6e195e6f448e31dc8c9c2
Samuel <127067590+Panda5m@users.noreply.github.com>
2023-05-23 08:49:41 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 class User(db.model, UserMixing):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     emailid = Column(String(100), nullable=False, unique=True)
     password_hash = Column(String(255), nullable=False)
@@ -14,7 +14,7 @@ class User(db.model, UserMixing):
 class Event(db.model):
     __tablename__ = 'events'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     event_title = Column(String(80))
     date = Column(Date)
     start_time = Column(Time, nullable=False)
