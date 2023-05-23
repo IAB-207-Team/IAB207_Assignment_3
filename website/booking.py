da9e1edca8d3de2cbba6a06fc42b41533a89293a
Theguy4126 <106867846+Theguy4126@users.noreply.github.com>
2023-05-23 10:38:57 +1000

    Update booking.py

--- website/booking.py
+++ website/booking.py
@@ -1,27 +1,68 @@
-#This is just something I quickly made up, but I might be useful for getting the ticketing system to work
-from flask import Flask, render_template, request
-import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required, current_user, UserMixin
 
-@app.route('/events/<event_id>/book', methods=['GET', 'POST'])
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_management.db'
+app.config['SECRET_KEY'] = 'your-secret-key'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Define Event model
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ticket_count = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    events = Event.query.all()
+    return render_template('index.html', events=events)
+
+@app.route('/events/<int:event_id>', methods=['GET'])
+def event_details(event_id):
+    event = Event.query.get(event_id)
+
+    if event is None:
+        return 'Event not found', 404
+
+    return render_template('event_details.html', event=event)
+
+@app.route('/events/<int:event_id>/book', methods=['GET', 'POST'])
+@login_required
 def book_event(event_id):
-    cursor.execute('SELECT * FROM events WHERE id = ?', (event_id,))
-    event = cursor.fetchone()
+    event = Event.query.get(event_id)
 
     if event is None:
         return 'Event not found', 404
 
     if request.method == 'POST':
-        if event == 0:
-            cursor.execute('UPDATE events SET status = ? WHERE id = ?', ('Sold Out', event_id))
-            conn.commit()
+        if event.ticket_count == 0:
+            event.status = 'Sold Out'
+            db.session.commit()
             return 'Event is sold out'
 
-        new_ticket_count = event - 1
-        cursor.execute('UPDATE events SET ticket_count = ? WHERE id = ?', (new_ticket_count, event_id))
+        quantity = int(request.form['quantity'])
+        if quantity > event.ticket_count:
+            return 'Order cannot be placed. The quantity exceeds the available tickets.'
+
+        new_ticket_count = event.ticket_count - quantity
+        event.ticket_count = new_ticket_count
         if new_ticket_count == 0:
-            cursor.execute('UPDATE events SET status = ? WHERE id = ?', ('Sold Out', event_id))
-        conn.commit()
+            event.status = 'Sold Out'
+        db.session.commit()
 
         return 'Ticket booked successfully'
 
     return render_template('book_event.html', event=event)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
