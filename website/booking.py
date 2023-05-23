709f9d6fe2c7f6d7ffa126424ff34132099ad548
Theguy4126 <106867846+Theguy4126@users.noreply.github.com>
2023-05-23 10:39:43 +1000

    Update booking.py

--- website/booking.py
+++ website/booking.py
@@ -2,14 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, current_user, UserMixin
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_management.db'
-app.config['SECRET_KEY'] = 'your-secret-key'
-
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
 # Define Event model
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
