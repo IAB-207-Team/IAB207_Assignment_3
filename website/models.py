9a174c4a6c56242c36bea6cf6453f973675b1aed
Samuel <127067590+Panda5m@users.noreply.github.com>
2023-05-23 10:10:38 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -2,19 +2,19 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 
-class User(db.model, db.UserMixing):
+class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    emailid = db.Column(db.String(100), nullable=False, unique=True)
+    emailid = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    user = db.relationship('Comment', backref='User')
 
-
-class Event(db.model):
+class Event(db.Model):
     __tablename__ = 'events'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     event_title = db.Column(db.String(80))
     date = db.Column(db.Date)
     start_time = db.Column(db.Time, nullable=False)
@@ -23,13 +23,14 @@ class Event(db.model):
     genre = db.Column(db.String(255))
     location = db.Column(db.String(255))
     Amount_of_Tickets = db.Column(db.Integer)
-    event_Status = db.Column(db.String(255), default='Upcoming Event')
-    ticket_Price = db.Column(db.Integer)
+    Ticket_Status = db.Column(db.String(255))
+    Ticket_Price = db.Column(db.Integer)
     image = db.Column(db.String(400))
-
-    comments = db.relationship('Comment', backref='event')
     
-class Comment(db.model):
+    comments = db.relationship('Comment', backref='Event')
+    event_booking = db.relationship('Booking', backref='Event')
+
+class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -38,19 +39,20 @@ class Comment(db.model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
-    user = db.relationship('User', backref='comments')
 
-#Need to Add a booked table to keep track of tickets
-class Booking(db.model):
+class Booking(db.Model):
     __tablename__ = 'booking'
     
-    order_id = db.Column(db.Integer, autoincrement=True)
+    order_id = db.Column(db.Integer, autoincrement=True, primary_key =True)
     order_date = db.Column(db.Date)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
     user_booking = db.relationship('User', backref='booking')
     event_booking = db.relationship('Event', backref='booking')
+
+    def __repr__(self):
+        return "<Comment: {}>".format(self.text)
 
     def __repr__(self):
         return "<Comment: {}>".format(self.text)
