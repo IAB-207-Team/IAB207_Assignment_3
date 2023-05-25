a528ae4cd27539ac60e3cb6c4d516d560d6007f0
n11283173 <104541077+n11283173@users.noreply.github.com>
2023-05-25 20:21:11 +1000

    Updated views for search function

--- website/views.py
+++ website/views.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template, request, redirect,url_for
-from .models import booking
+from .models import Event
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    event = booking.query.all()    
+    event = Event.query.all()    
     return render_template('index.html', event=event)
 
 #Search Funcationality 
@@ -14,7 +14,7 @@ def search():
     if request.args['search']:
         print(request.args['search'])
         dest = "%" + request.args['search'] + '%'
-        event = booking.query.filter(booking.description.like(booking)).all()
+        event = Event.query.filter(Event.description.like(Event)).all()
         return render_template('index.html', event=event)
     else:
         return redirect(url_for('main.index'))
