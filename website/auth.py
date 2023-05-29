d830ca7633b17f38a8a58874b08884ed839e50e6
Samuel <127067590+Panda5m@users.noreply.github.com>
2023-05-30 09:41:23 +1000

    Added, Backend for creating event

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request,redirect,url_for,flash
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, CreateEventForm, CommentForm
 #new imports:
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash,check_password_hash
-from .models import User
+from .models import User, Event, Comment
+import os
+from werkzeug.utils import secure_filename
 from . import db
 
 #create a blueprint
@@ -64,3 +66,65 @@ def login(): #view function
 def logout():
     logout_user()
     return 'You have been logged out'
+
+@bp.route('/<id>')
+def show(id):
+    destination = Event.query.filter_by(id=id).first()
+    # create the comment form
+    cform = CommentForm()    
+    return render_template('eventdesc.html', destination=destination, form=cform)
+
+@bp.route('/create', methods = ['GET', 'POST'])
+@login_required
+def create():
+  print('Method type: ', request.method)
+  form = CreateEventForm()
+  if form.validate_on_submit():
+    #call the function that checks and returns image
+    db_file_path=check_upload_file(form)
+    destination=Event(name=form.event_title.data, date=form.date.data, start_time=form.start_time.data,end_time=form.end_time.data, description=form.description.data, genre=form.genre.data, amount_of_tickets=form.amount_of_tickets.data, ticket_price=form.ticket_price.data,image=form.image.data)
+    # add the object to the db session
+    db.session.add(destination)
+    # commit to the database
+    db.session.commit()
+    print('Successfully created new travel destination', 'success')
+    #Always end with redirect when form is valid
+    return redirect(url_for('auth.create'))
+  return render_template('eventcreate.html', form=form)
+
+def check_upload_file(form):
+  #get file data from form  
+  fp=form.image.data
+  filename=fp.filename
+  #get the current path of the module file… store image file relative to this path  
+  BASE_PATH=os.path.dirname(__file__)
+  #upload file location – directory of this file/static/image
+  upload_path=os.path.join(BASE_PATH,'static/image',secure_filename(filename))
+  #store relative path in DB as image location in HTML is relative
+  db_upload_path='/static/image/' + secure_filename(filename)
+  #save the file and return the db upload path  
+  fp.save(upload_path)
+  return db_upload_path
+
+@bp.route('/<destination>/comment', methods = ['GET', 'POST'])  
+@login_required
+def comment(destination):  
+    form = CommentForm()  
+    #get the destination object associated to the page and the comment
+    destination_obj = Event.query.filter_by(id=destination).first()  
+    if form.validate_on_submit():  
+      #read the comment from the form
+      comment = Comment(text=form.text.data,  
+                        destination=destination_obj,
+                        user=current_user) 
+      #here the back-referencing works - comment.destination is set
+      # and the link is created
+      db.session.add(comment) 
+      db.session.commit() 
+
+      #flashing a message which needs to be handled by the html
+      #flash('Your comment has been added', 'success')  
+      print('Your comment has been added', 'success') 
+    # using redirect sends a GET request to destination.show
+    return redirect(url_for('auth.show', id=destination))
+
