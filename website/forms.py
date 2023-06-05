970ecbfc367708d2ebeceabd2eb3ab64bf6820a3
Samuel <127067590+Panda5m@users.noreply.github.com>
2023-06-05 21:23:07 +1000

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -8,14 +8,14 @@ ALLOWED_FILE = {'PNG','JPG','png','jpg'}
 #Create new event
 class CreateEvent(FlaskForm):
     event_title = StringField('Event Title', validators=[InputRequired()])
-    date = DateField('Start Date', format='%m/%d/%Y')
+    date = DateField('Start Date', format='%Y-%m-%d')
     start_time = TimeField('Start Time')
     end_time = TimeField('End Time')
     description = TextAreaField('Description', validators=[InputRequired()])
     genre = StringField('Genre')
     location = TextAreaField('Mailing Address', validators=[InputRequired()])
     amount_of_tickets = IntegerField('Amount')
-    ticket_price =DecimalField('Price', places=2, render_kw={'TIcket Price': 'Enter the ticket price'})
+    ticket_price =IntegerField('Price', render_kw={'TIcket Price': 'Enter the ticket price'})
     image = FileField('Destination Image', validators=[FileRequired(message='Image cannot be empty'), FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
     submit = SubmitField('Create')
     
@@ -23,7 +23,7 @@ class BookEvent(FlaskForm):
     email_id=StringField("Email Address", validators=[InputRequired('Enter email')])
     card_no = IntegerField('Card Number')
     quantity = IntegerField('Quantity')
-    expiry = DateField('Expiry Date', format='%m/%Y')
+    expiry = DateField('Expiry Date', format='%Y-%m')
     CVV =  IntegerField('Security Code')
     
     #We are going to have to make sure that the event idea automatically gets added to the booking
